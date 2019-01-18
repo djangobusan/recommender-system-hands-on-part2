03cda02d2f553f2a0b9f29bf92fa74febd8f04ad
sigmadream <sigmadream@gmail.com>
2019-01-19 05:27:06 +0900

    Added 로그아웃 처리

--- settings/urls.py
+++ settings/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
+    path('logout/', views.user_logout, name='Logout'),
 ]
